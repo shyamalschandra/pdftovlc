e77ed18f898e5bebc8902d9acf9b4e4ee0978471
Shyamal S. Chandra <shyamalc@icloud.com>
2019-07-31 10:58:23 -0500

    Update pdftovlc.sh

--- pdftovlc.sh
+++ pdftovlc.sh
@@ -5,12 +5,13 @@ export MUSIC_FILE=`echo $FILE | cut -d "." -f1| awk '{print $0".aiff"}'`
 export ASCII_TEXT_FILE=`echo $FILE | cut -d "." -f1| awk '{print $0"-ascii.txt"}'`
 export NODOTS_TEXT_FILE=`echo $FILE | cut -d "." -f1| awk '{print $0"-nodots.txt"}'`
 export FINISHED_MUSIC_FILE=`echo $FILE | cut -d "." -f1| awk '{print $0".mp3"}'`
+export SED_STRING=`s/\s.\\g`
 echo "Convert PDF to text"
 pdftotext $FILE $TEXT_FILE
 echo "Remove Unicode parts"
 echo `iconv -c -f utf-8 -t ascii $TEXT_FILE` > $ASCII_TEXT_FILE
 echo "Remove Dots"
-cat $ASCII_TEXT_FILE | sed 's/\s.\\g' > $NODOTS_TEXT_FILE
+cat $ASCII_TEXT_FILE | sed $SED_STRING > $NODOTS_TEXT_FILE
 echo "Converting from text to AIFF"
 say --progress -r 300 -o $MUSIC_FILE -f $NODOTS_TEXT_FILE
 echo "Converting from AIFF to MP3"
