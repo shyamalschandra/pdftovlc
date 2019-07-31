e4c080f54551cb05d408ad868e44e17b5d1badb8
Shyamal S. Chandra <shyamalc@icloud.com>
2019-07-31 10:51:59 -0500

    removing the dots

--- pdftovlc.sh
+++ pdftovlc.sh
@@ -3,13 +3,16 @@ export FILE=`ls -Art *.pdf | tail -n 1`
 export TEXT_FILE=`echo $FILE | cut -d "." -f1| awk '{print $0".txt"}'`
 export MUSIC_FILE=`echo $FILE | cut -d "." -f1| awk '{print $0".aiff"}'`
 export ASCII_TEXT_FILE=`echo $FILE | cut -d "." -f1| awk '{print $0"-ascii.txt"}'`
+export NODOTS_TEXT_FILE=`echo $FILE | cut -d "." -f1| awk '{print $0"-nodots.txt"}'`
 export FINISHED_MUSIC_FILE=`echo $FILE | cut -d "." -f1| awk '{print $0".mp3"}'`
 echo "Convert PDF to text"
 pdftotext $FILE $TEXT_FILE
 echo "Remove Unicode parts"
 echo `iconv -c -f utf-8 -t ascii $TEXT_FILE` > $ASCII_TEXT_FILE
+echo "Remove Dots"
+cat $ASCII_TEXT_FILE | sed 's/\s/.\\g' > $NODOTS_TEXT_FILE
 echo "Converting from text to AIFF"
-say --progress -r 300 -o $MUSIC_FILE -f $ASCII_TEXT_FILE
+say --progress -r 300 -o $MUSIC_FILE -f $NODOTS_TEXT_FILE
 echo "Converting from AIFF to MP3"
 ffmpeg -i $MUSIC_FILE $FINISHED_MUSIC_FILE
 echo "Uploading MP3 to iOS device via VLC mobile app"
