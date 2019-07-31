c5818c22b2edea620f8c725cde4e8180b4a06bd7
Shyamal S. Chandra <shyamalc@icloud.com>
2019-07-31 12:09:24 -0500

    Update pdftovlc.sh

--- pdftovlc.sh
+++ pdftovlc.sh
@@ -5,7 +5,7 @@ export MUSIC_FILE=`echo $FILE | cut -d "." -f1| awk '{print $0".aiff"}'`
 export ASCII_TEXT_FILE=`echo $FILE | cut -d "." -f1| awk '{print $0"-ascii.txt"}'`
 export NODOTS_TEXT_FILE=`echo $FILE | cut -d "." -f1| awk '{print $0"-nodots.txt"}'`
 export FINISHED_MUSIC_FILE=`echo $FILE | cut -d "." -f1| awk '{print $0".mp3"}'`
-SED_STRING="'s/\\s.\\\\g'"
+SED_STRING="\'s/\\s.\\\\g\'"
 echo "Convert PDF to text"
 pdftotext $FILE $TEXT_FILE
 echo "Remove Unicode parts"
